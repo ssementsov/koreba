d6613b2b36ea2253cd58e2d31b5c02ce6a047175
Evgeny <64806042+EvgenyKoreba@users.noreply.github.com>
2021-12-19 15:15:02 +0300

    2-th task solution

---
+++ Course/02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = Convert.ToInt32(Console.ReadLine());
+            int sum = Convert.ToInt32(Console.ReadLine());
+            int second = sum - first;
+
+            Console.WriteLine(sum + "=" + first + "+" + second);
+            Console.ReadKey();
+        }
+    }
+}
