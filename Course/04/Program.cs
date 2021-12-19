fc1d36e0e9a173e45ba3fb11d75688dc7cce46b7
Evgeny <64806042+EvgenyKoreba@users.noreply.github.com>
2021-12-19 15:36:18 +0300

    4-th task solution

---
+++ Course/04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string template = "Background color set to ";
+            Array colors = Enum.GetValues(typeof(ConsoleColor));
+
+            foreach (ConsoleColor color in colors)
+            {
+                Console.BackgroundColor = color;
+                Console.WriteLine(template + color.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
