2839a60e281f6c7abfefcbde416406d004cb5b24
Evgeny <64806042+EvgenyKoreba@users.noreply.github.com>
2021-12-28 00:05:31 +0300

    Solution of 8 - 10 tasks

---
+++ Course/09/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float variable = 2.7182818284590452f;
+
+            Byte b = (Byte)variable;
+            Console.WriteLine($"byte {b}");
+
+            SByte sb = (SByte)variable;
+            Console.WriteLine($"sByte {sb}");
+
+            Int16 sh = (Int16)variable;
+            Console.WriteLine($"short {sh}");
+
+            UInt16 ush = (UInt16)variable;
+            Console.WriteLine($"ushort {ush}");
+
+            Int32 i = (Int32)variable;
+            Console.WriteLine($"int {i}");
+
+            UInt32 ui = (UInt32)variable;
+            Console.WriteLine($"uInt {ui}");
+
+            Int64 L = (Int64)variable;
+            Console.WriteLine($"long {L}");
+
+            UInt64 uL = (UInt64)variable;
+            Console.WriteLine($"uLong {uL}");
+
+            Double d = (Double)variable;
+            Console.WriteLine($"Double {d}");
+
+            Decimal dec = (Decimal)variable;
+            Console.WriteLine($"Decimal {dec}");
+
+            Single s = (Single)variable;
+            Console.WriteLine($"Float {s}");
+
+            Console.Read();
+        }
+    }
+}
---
+++ Course/10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Double.NaN / 2);
+            Console.WriteLine(0f / 0f);
+            Console.WriteLine(Double.PositiveInfinity / Double.PositiveInfinity);
+            Console.WriteLine(Double.PositiveInfinity * 0);
+            Console.WriteLine(Double.PositiveInfinity + Double.NegativeInfinity);
+            Console.WriteLine(Math.Sqrt(-25));
+            Console.WriteLine(Math.Log10(-25));
+
+            Console.ReadKey();
+        }
+    }
+}
