6d52bae92c99339e4810316f160074644819ae58
Evgeny <64806042+EvgenyKoreba@users.noreply.github.com>
2022-01-12 00:07:44 +0300

    Solution of task 037

---
+++ Course/037/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _037
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,,] library = new string[5, 10, 10];
+
+            library[1, 2, 3] = "Book1";
+            library[2, 3, 4] = "Book2";
+            library[4, 5, 6] = "Book3";
+
+            Console.Write("Enter the title of the book: ");
+            string book = Console.ReadLine();
+
+            FindBook(library, book);
+
+            Console.ReadKey();
+        }
+
+        private static void FindBook(string[,,] library ,string book)
+        {
+            for (int z = 0; z < library.GetLength(0); z++)
+            {
+                for (int y = 0; y < library.GetLength(1); y++)
+                {
+                    for (int x = 0; x < library.GetLength(2); x++)
+                    {
+                        if (library[z,y,x] == book)
+                        {
+                            Console.WriteLine($"The book {book} is numbered on the rack {z}, shelf {y}, place {x}");
+                            return;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"The book {book} is not found in library.");
+        }
+    }
+}
---
+++ Course/037/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("037")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("037")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("436cf601-3438-4b32-8361-84ea74f2f00c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
