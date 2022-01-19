de109daa1a5b3f58fb9e30ef23f8e6eb0148d265
Evgeny <64806042+EvgenyKoreba@users.noreply.github.com>
2022-01-19 22:21:18 +0300

    Solution of 045, 046 and 047 tasks

--- Course/045/Properties/AssemblyInfo.cs
+++ Course/045/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("041")]
+[assembly: AssemblyTitle("045")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("041")]
+[assembly: AssemblyProduct("045")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fd4699d2-3c3e-4692-8333-a72076cb8098")]
+[assembly: Guid("1c888ae3-e8da-4c01-b80c-bf5a041ab7d0")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Course/046/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task_046
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the string: ");
+            string origin = Console.ReadLine();
+
+            string resultedString = GetReversedStringWithUniqueSymbols(origin);
+            Console.WriteLine(resultedString);
+
+            Console.ReadKey();
+        }
+
+        private static string GetReversedStringWithUniqueSymbols(string origin)
+        {
+            string resultedString = origin[origin.Length - 1].ToString();
+
+            char[] symbols = origin.ToCharArray();
+
+            for (int i = symbols.Length - 2; i >= 0; i--)
+            {
+                if (IsNotContainChar(resultedString, symbols[i]))
+                {
+                    resultedString += symbols[i].ToString();
+                }
+            }
+
+            return resultedString;
+        }
+
+        private static bool IsNotContainChar(string original, char symbol)
+        {
+            bool isNotContain = true;
+
+            for (int i = 0; i < original.Length; i++)
+            {
+                if (original[i] == symbol)
+                {
+                    isNotContain = false;
+                    break;
+                }
+            }
+
+            return isNotContain;
+        }
+    }
+}
