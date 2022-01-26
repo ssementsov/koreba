c235146d8f28c422be51895c83e8776eb404a2f6
Evgeny <64806042+EvgenyKoreba@users.noreply.github.com>
2022-01-27 00:33:00 +0300

    Solution of task 0107 (MyStack)

---
+++ OOP/0107/MyStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task_0107
+{
+    public class MyStack
+    {
+        private int index = 0;
+        private int[] _stack;
+
+        public MyStack(uint size)
+        {
+            _stack = new int[size];
+        }
+
+        public int Pop()
+        {
+            if (index >= 0)
+            {
+                int number = _stack[index - 1];
+                _stack[index - 1] = default;
+                index--;
+
+                return number;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Stack is empty");
+            }
+        }
+
+        public void Push(int number)
+        {
+            if (index < _stack.Length)
+            {
+                _stack[index] = number;
+                index++;
+            }
+            else
+            {
+                throw new StackOverflowException();
+            }
+        }
+    }
+}
---
+++ OOP/0107/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_0107
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stack = new MyStack(5);
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            //stack.Push(6);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            //Console.WriteLine(stack.Pop());
+
+            Console.ReadKey();
+        }
+    }
+}
