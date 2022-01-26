92c3182d0c9ba601539620e39f1bddadf1484d7c
Evgeny <64806042+EvgenyKoreba@users.noreply.github.com>
2022-01-26 23:38:05 +0300

    Solution of 048 and 053 tasks

---
+++ Course/048/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task_048
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your text:");
+            string text = Console.ReadLine();
+
+            string[][] savedText = SaveText(text);
+            PrintArray(savedText);
+
+            Console.ReadKey();
+        }
+
+        private static string[][] SaveText(string text)
+        {
+            string[] sentences = text.Trim().Split('.');
+            string[][] savedText = new string[sentences.Length][];
+
+            for (int i = 0; i < savedText.Length; i++)
+            {
+                savedText[i] = sentences[i].Split(' ');
+            }
+
+            return savedText;
+        }
+
+        private static void PrintArray<T>(T[][] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    Console.Write($"{array[i][j]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Course/053/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Task_053
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write($"{"Enter first number:",-23}");
+            uint first = uint.Parse(Console.ReadLine());
+
+            Console.Write($"{"Enter second number:",-23}");
+            uint second = uint.Parse(Console.ReadLine());
+
+            uint number = GreatestCommonDivisor(first, second);
+
+            Console.WriteLine(number);
+            Console.ReadKey();
+        }
+
+        private static uint GreatestCommonDivisor(uint m, uint n)
+        {
+            uint min = Math.Min(m, n);
+
+            for (uint i = 2; i <= min; i++)
+            {
+                if (m % i == 0 && n % i == 0)
+                {
+                    return i * GreatestCommonDivisor(m / i, n / i);
+                }
+            }
+
+            return 1;
+        }
+
+        //private static uint GreatestCommonDivisor(uint m, uint n)
+        //{
+        //    uint result = 1;
+        //    uint min = Math.Min(m, n);
+
+        //    for (uint i = min; i >= 2; i--)
+        //    {
+        //        if (m % i == 0 && n % i == 0)
+        //        {
+        //            result = n;
+        //        }
+        //    }
+
+        //    return result;
+        //}
+    }
+}
