8695a50cc4e67ac2e25e996b222b96285d75149b
sweetYdnac <96253105+sweetYdnac@users.noreply.github.com>
2022-03-09 21:19:00 +0300

    Lesson020

---
+++ Classwork/Lesson020/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Lesson020
+{
+    class Program
+    {
+        static void Main()
+        {
+            //object obj1 = new String("str");
+            //object obj2 = new String("str");
+            //string str1 = new String("str");
+            //string str2 = new String("Str");
+
+            //Console.WriteLine($"Objects is equals - {obj1 == obj2}");
+            //Console.WriteLine($"Strings is equals - {str1 == str2}");
+
+            //--------------- REGEX -------------------
+
+            //string s = "погода автопад мобиль Автомобиль гора гораАвтомат";
+            //Regex reg = new Regex(@"(\w*)(а|А)вто(\w*)");
+
+            //var match = reg.Matches(s);
+
+            //foreach (var m in match)
+            //{
+            //    Console.WriteLine(m);
+            //}
+
+            //------------- FILESTREAM -------------------
+            var s = new StreamExample();
+            s.DoWork();
+        }
+    }
+}
---
+++ Classwork/Lesson020/StreamExample.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Lesson020
+{
+    internal class StreamExample
+    {
+        public void DoWork()
+        {
+            string path = @"SomeDir";
+            DirectoryInfo dir = new DirectoryInfo(path);
+
+            if (!dir.Exists)
+            {
+                dir.Create();
+            }
+
+            var file = new FileInfo($"{path}/note.txt");
+
+            using (FileStream fs = file.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                byte[] bArr = Encoding.Default.GetBytes("My name Sergei!");
+                fs.Write(bArr, 0, bArr.Length);
+            }
+
+            using(FileStream fs = file.OpenRead())
+            {
+                byte[] data = new byte[fs.Length];
+                fs.Read(data, 0, data.Length);
+                string str = Encoding.Default.GetString(data);
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
